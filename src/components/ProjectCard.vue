a386841b8dbec14a7bd29a62ff4c157f77a08366
willsondev <willsondev12@gmail.com>
2024-09-23 22:55:50 -0300

    add videos

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,29 +1,61 @@
 <template>
-  <div class=" text-white p-4 rounded-lg shadow-lg hover:shadow-xl transition duration-300">
-    <img :src="imageSrc" alt="project-image" />
+  <div class="text-white p-4 rounded-lg shadow-lg hover:shadow-xl transition duration-300">
+    <div class="relative">
+      <!-- Imagen que se mostrará por defecto -->
+      <img
+        v-if="!isHovered"
+        :src="imageSrc"
+        alt="project-image"
+        class="w-full rounded-t-lg"
+        @mouseover="isHovered = true"
+        @mouseleave="isHovered = false"
+      />
+      <!-- Video que se mostrará al pasar el cursor -->
+      <video
+        v-if="isHovered"
+        :src="videoSrc"
+        class="w-full rounded-t-lg"
+        muted
+        preload="auto"
+        playsinline
+        @mouseenter="playVideo"
+        @mouseleave="pauseVideo"
+        ref="video" 
+      ></video>
+    </div>
     <div class="p-6">
-      <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+      <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900 dark:text-white">
         {{ title }}
       </h5>
-      <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+      <p class="block font-sans text-base antialiased font-light leading-relaxed text-gray-300">
         {{ description }}
       </p>
+      <h5 class="font-bold mt-4">Tecnologías:</h5>
+      <div class="flex flex-row space-x-2 items-start mt-2 mr-32">
+        <span v-html="icon1" class="h-10 w-10"></span>
+        <span v-html="icon2" class="h-10 w-10" v-if="icon2"></span>
+        <span v-html="icon3" class="h-10 w-10"></span>
+        <span v-html="icon4" class="h-10 w-10"></span>
+        <span v-html="icon5" class="h-10 w-10"></span>
+        <span v-html="icon6" class="h-10 w-10"></span>
+        <span v-html="icon7" class="h-10 w-10"></span>
+      </div>
     </div>
-    <div class="p-4 space-x-2 pt-0">
+    <div class="p-4 space-x-2 pt-0 flex justify-center">
       <button
-        class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+        class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md hover:shadow-lg"
         type="button"
         @click="openGitHub"
       >
-        <i class="fab fa-github me-2"></i> GitHub
+        <i class="fab fa-github me-2"></i> Codigo
       </button>
 
       <button
-        class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+        class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md hover:shadow-lg"
         type="button"
         @click="openProyecto"
       >
-        Proyecto <i class="fa-solid fa-arrow-right"></i>
+        DEMO <i class="fa-solid fa-arrow-right"></i>
       </button>
     </div>
   </div>
@@ -36,6 +68,10 @@ export default {
       type: String,
       required: true
     },
+    videoSrc: {
+      type: String,
+      required: true
+    },
     title: {
       type: String,
       required: true
@@ -43,7 +79,40 @@ export default {
     description: {
       type: String,
       required: true
+    },
+    icon1: {
+      type: String,
+      required: true
+    },
+    icon2: {
+      type: String,
+      required: false
+    },
+    icon3: {
+      type: String,
+      required: true
+    },
+    icon4: {
+      type: String,
+      required: true
+    },
+    icon5: {
+      type: String,
+      required: true
+    },
+    icon6: {
+      type: String,
+      required: true
+    },
+    icon7: {
+      type: String,
+      required: true
     }
+  },
+  data() {
+    return {
+      isHovered: false
+    };
   },
   methods: {
     openGitHub() {
@@ -51,7 +120,27 @@ export default {
     },
     openProyecto() {
       window.open('https://frontend-arqueria.vercel.app/', '_blank');
+    },
+    playVideo() {
+      const video = this.$refs.video; // Accedemos al video usando ref
+      if (video) {
+        video.play(); // Reproduce el video
+      }
+    },
+    pauseVideo() {
+      const video = this.$refs.video; // Accedemos al video usando ref
+      if (video) {
+        video.pause(); // Pausa el video
+        video.currentTime = 0; // Reinicia el video
+      }
+      this.isHovered = false; // Cambia el estado de hovered
     }
   }
 }
 </script>
+
+<style scoped>
+.relative {
+  position: relative;
+}
+</style>
--- src/views/ProyectosView.vue
+++ src/views/ProyectosView.vue
@@ -9,8 +9,25 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 ">
       <ProjectCard
   imageSrc="/img/arqueria.png"
+   videoSrc="./img/pc.mp4"
   title="Clases de Arquería"
   description="Aplicación web para reservar clases de arquería con autenticación de usuarios y administración de contenido. Desarrollada con Vue.js."
+  
+  :icon7="vuerouterIcon"
+  :icon4="nodeIcon"
+  :icon5="expressIcon"
+  :icon6="mongoIcon"
+ 
+/>
+
+ <ProjectCard
+  imageSrc="/img/iniciobarber.png"
+   videoSrc="/img/iphone2.mp4"
+  title="BARBERSHOP"
+  description="Aplicación web con Vue.js y Firebase para gestión de barbería. Incluye reservas en línea, autenticación de usuarios y panel de administración para gestionar servicios, todo con un diseño moderno y rutas protegidas"
+   :icon1="vueIcon"
+        :icon2="firebaseIcon"
+        :icon3="tailwindIcon"
 />
 
       
@@ -26,10 +43,77 @@ import ProjectCard from '@/components/ProjectCard.vue';
 export default {
   components: {
     ProjectCard
+  },
+  data() {
+    return {
+       vuerouterIcon: `
+        <svg width="40" height="40" viewBox="0 0 256 221" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid">
+          <path d="M204.8 0H256L128 220.8 0 0h97.92L128 51.2 157.44 0h47.36Z" fill="#41B883"/>
+          <path d="m0 0 128 220.8L256 0h-51.2L128 132.48 50.56 0H0Z" fill="#41B883"/>
+          <path d="M50.56 0 128 133.12 204.8 0h-47.36L128 51.2 97.92 0H50.56Z" fill="#35495E"/>
+        </svg>
+      `,
+      vueIcon: `
+        <svg width="40" height="40" viewBox="0 0 256 221" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid">
+          <path d="M204.8 0H256L128 220.8 0 0h97.92L128 51.2 157.44 0h47.36Z" fill="#41B883"/>
+          <path d="m0 0 128 220.8L256 0h-51.2L128 132.48 50.56 0H0Z" fill="#41B883"/>
+          <path d="M50.56 0 128 133.12 204.8 0h-47.36L128 51.2 97.92 0H50.56Z" fill="#35495E"/>
+        </svg>
+      `,
+      firebaseIcon: `
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 600 600">
+          <path fill="#FF9100" d="M213.918 560.499c23.248 9.357 48.469 14.909 74.952 15.834 35.84 1.252 69.922-6.158 100.391-20.234-36.537-14.355-69.627-35.348-97.869-61.448-18.306 29.31-45.382 52.462-77.474 65.848Z"/>
+          <path fill="#FFC400" d="M291.389 494.66c-64.466-59.622-103.574-145.917-100.269-240.568.108-3.073.27-6.145.46-9.216a166.993 166.993 0 0 0-36.004-5.241 167.001 167.001 0 0 0-51.183 6.153c-17.21 30.145-27.594 64.733-28.888 101.781-3.339 95.611 54.522 179.154 138.409 212.939 32.093-13.387 59.168-36.51 77.475-65.848Z"/>
+          <path fill="#FF9100" d="M291.39 494.657c14.988-23.986 24.075-52.106 25.133-82.403 2.783-79.695-50.792-148.251-124.942-167.381-.19 3.071-.352 6.143-.46 9.216-3.305 94.651 35.803 180.946 100.269 240.568Z"/>
+          <path fill="#DD2C00" d="M308.231 20.858C266 54.691 232.652 99.302 212.475 150.693c-11.551 29.436-18.81 61.055-20.929 94.2 74.15 19.13 127.726 87.686 124.943 167.38-1.058 30.297-10.172 58.39-25.134 82.404 28.24 26.127 61.331 47.093 97.868 61.447 73.337-33.9 125.37-106.846 128.383-193.127 1.952-55.901-19.526-105.724-49.875-147.778-32.051-44.477-159.5-194.36-159.5-194.36Z"/>
+        </svg>
+      `,
+      tailwindIcon: `
+      <svg class="mx-auto"
+  viewBox="0 0 256 154"
+  width="40"
+  height="40"
+  xmlns="http://www.w3.org/2000/svg"
+  preserveAspectRatio="xMidYMid"
+  >
+  <defs
+    ><linearGradient x1="-2.778%" y1="32%" x2="100%" y2="67.556%" id="gradient">
+      <stop stop-color="#2298BD" offset="0%"></stop>
+      <stop stop-color="#0ED7B5" offset="100%"></stop>
+    </linearGradient></defs>
+  <path
+    d="M128 0C93.867 0 72.533 17.067 64 51.2 76.8 34.133 91.733 27.733 108.8 32c9.737 2.434 16.697 9.499 24.401 17.318C145.751 62.057 160.275 76.8 192 76.8c34.133 0 55.467-17.067 64-51.2-12.8 17.067-27.733 23.467-44.8 19.2-9.737-2.434-16.697-9.499-24.401-17.318C174.249 14.743 159.725 0 128 0ZM64 76.8C29.867 76.8 8.533 93.867 0 128c12.8-17.067 27.733-23.467 44.8-19.2 9.737 2.434 16.697 9.499 24.401 17.318C81.751 138.857 96.275 153.6 128 153.6c34.133 0 55.467-17.067 64-51.2-12.8 17.067-27.733 23.467-44.8 19.2-9.737-2.434-16.697-9.499-24.401-17.318C110.249 91.543 95.725 76.8 64 76.8Z"
+    fill="url(#gradient)"></path></svg>`,
+      nodeIcon: `
+      <svg viewBox="0 0 256 292" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="40" height="40"><defs><linearGradient id="a" x1="68.188%" x2="27.823%" y1="17.487%" y2="89.755%"><stop offset="0%" stop-color="#41873F"/><stop offset="32.88%" stop-color="#418B3D"/><stop offset="63.52%" stop-color="#419637"/><stop offset="93.19%" stop-color="#3FA92D"/><stop offset="100%" stop-color="#3FAE2A"/></linearGradient><linearGradient id="c" x1="43.277%" x2="159.245%" y1="55.169%" y2="-18.306%"><stop offset="13.76%" stop-color="#41873F"/><stop offset="40.32%" stop-color="#54A044"/><stop offset="71.36%" stop-color="#66B848"/><stop offset="90.81%" stop-color="#6CC04A"/></linearGradient><linearGradient id="f" x1="-4.389%" x2="101.499%" y1="49.997%" y2="49.997%"><stop offset="9.192%" stop-color="#6CC04A"/><stop offset="28.64%" stop-color="#66B848"/><stop offset="59.68%" stop-color="#54A044"/><stop offset="86.24%" stop-color="#41873F"/></linearGradient><path id="b" d="M134.923 1.832c-4.344-2.443-9.502-2.443-13.846 0L6.787 67.801C2.443 70.244 0 74.859 0 79.745v132.208c0 4.887 2.715 9.502 6.787 11.945l114.29 65.968c4.344 2.444 9.502 2.444 13.846 0l114.29-65.968c4.344-2.443 6.787-7.058 6.787-11.945V79.745c0-4.886-2.715-9.501-6.787-11.944L134.923 1.832Z"/><path id="e" d="M134.923 1.832c-4.344-2.443-9.502-2.443-13.846 0L6.787 67.801C2.443 70.244 0 74.859 0 79.745v132.208c0 4.887 2.715 9.502 6.787 11.945l114.29 65.968c4.344 2.444 9.502 2.444 13.846 0l114.29-65.968c4.344-2.443 6.787-7.058 6.787-11.945V79.745c0-4.886-2.715-9.501-6.787-11.944L134.923 1.832Z"/></defs><path fill="url(#a)" d="M134.923 1.832c-4.344-2.443-9.502-2.443-13.846 0L6.787 67.801C2.443 70.244 0 74.859 0 79.745v132.208c0 4.887 2.715 9.502 6.787 11.945l114.29 65.968c4.344 2.444 9.502 2.444 13.846 0l114.29-65.968c4.344-2.443 6.787-7.058 6.787-11.945V79.745c0-4.886-2.715-9.501-6.787-11.944L134.923 1.832Z"/><mask id="d" fill="#fff"><use xlink:href="#b"/></mask><path fill="url(#c)" d="M249.485 67.8 134.65 1.833c-1.086-.542-2.443-1.085-3.529-1.357L2.443 220.912c1.086 1.357 2.444 2.443 3.8 3.258l114.834 65.968c3.258 1.9 7.059 2.443 10.588 1.357L252.47 70.515c-.815-1.086-1.9-1.9-2.986-2.714Z" mask="url(#d)"/><mask id="g" fill="#fff"><use xlink:href="#e"/></mask><path fill="url(#f)" d="M249.756 223.898c3.258-1.9 5.701-5.158 6.787-8.687L130.579.204c-3.258-.543-6.787-.272-9.773 1.628L6.786 67.53l122.979 224.238c1.628-.272 3.529-.815 5.158-1.63l114.833-66.239Z" mask="url(#g)"/></svg>`,
+      expressIcon: `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="40" height="40"><path fill="#fff" d="M32 24.795c-1.164.296-1.884.013-2.53-.957l-4.594-6.356-.664-.88-5.365 7.257c-.613.873-1.256 1.253-2.4.944l6.87-9.222-6.396-8.33c1.1-.214 1.86-.105 2.535.88l4.765 6.435 4.8-6.4c.615-.873 1.276-1.205 2.38-.883l-2.48 3.288-3.36 4.375c-.4.5-.345.842.023 1.325L32 24.795zM.008 15.427l.562-2.764C2.1 7.193 8.37 4.92 12.694 8.3c2.527 1.988 3.155 4.8 3.03 7.95H1.48c-.214 5.67 3.867 9.092 9.07 7.346 1.825-.613 2.9-2.042 3.438-3.83.273-.896.725-1.036 1.567-.78-.43 2.236-1.4 4.104-3.45 5.273-3.063 1.75-7.435 1.184-9.735-1.248C1 21.6.434 19.812.18 17.9c-.04-.316-.12-.617-.18-.92q.008-.776.008-1.552zm1.498-.38h12.872c-.084-4.1-2.637-7.012-6.126-7.037-3.83-.03-6.58 2.813-6.746 7.037z"/></svg>
+      `,
+      mongoIcon: `
+      <svg viewBox="0 0 256 549" xmlns="http://www.w3.org/2000/svg" width="40" height="40" preserveAspectRatio="xMidYMid"><path fill="#01EC64" d="M175.622 61.108C152.612 33.807 132.797 6.078 128.749.32a1.03 1.03 0 0 0-1.492 0c-4.048 5.759-23.863 33.487-46.874 60.788-197.507 251.896 31.108 421.89 31.108 421.89l1.917 1.28c1.704 26.234 5.966 63.988 5.966 63.988h17.045s4.26-37.54 5.965-63.987l1.918-1.494c.213.214 228.828-169.78 31.32-421.677Zm-47.726 418.05s-10.227-8.744-12.997-13.222v-.428l12.358-274.292c0-.853 1.279-.853 1.279 0l12.357 274.292v.428c-2.77 4.478-12.997 13.223-12.997 13.223Z"/></svg>
+      `
+    
+    
+  
+  
+  
+    }
   }
 };
 </script>
 
 <style scoped>
-/* No es necesario agregar estilos adicionales ya que estamos utilizando Tailwind CSS */
+.project-card {
+  /* Otros estilos que tengas */
+  padding: 1rem;
+  border: 1px solid #e2e8f0;
+  border-radius: 0.5rem;
+  background-color: #fff;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.icon-container {
+  display: flex;
+  justify-content: flex-start; /* Alinea los iconos a la izquierda */
+}
 </style>
